966cb0d47b64801b1716677b5b1a57e85b2ac272
jncro-iscteiul <jncro@iscte-iul.pt>
2020-05-13 11:50:41 +0100

    teste_maven

---
+++ maven_teste/testeid/src/test/java/teste1/testeid/AppTest.java
@@ -0,0 +1,18 @@
+package teste1.testeid;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+	
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
